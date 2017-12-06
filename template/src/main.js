78a86904367f2fa928e51f42333181a48d99d9f9
James <promethorn@outlook.com>
2017-12-06 11:03:29 -0600

    Added vuex support

--- template/src/main.js
+++ template/src/main.js
@@ -7,12 +7,14 @@ import App from './App'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 {{#router}}
 import router from './router'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 {{/router}}
+import store from './store'
 
 Vue.config.productionTip = false{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store,
   {{#router}}
   router,
   {{/router}}
