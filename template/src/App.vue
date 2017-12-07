f939343e97c1394a6535e1d40bbad6da37a6547f
James <promethorn@outlook.com>
2017-12-07 06:12:47 -0600

    Added Sakura.css and custom styling

--- template/src/App.vue
+++ template/src/App.vue
@@ -28,15 +28,20 @@ export default {
 </script>
 
 <style>
+@import '../assets/colors.css';
+@import '../assets/sakura.css';
+@import url('https://fonts.googleapis.com/css?family=Average|Average+Sans');
+html{
+  --ff-main: 'Average', serif;
+  --ff-alt: 'Average Sans', sans-serif;
+}
 body {
   margin: 0;
 }
 
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
 }
 
 main {
@@ -48,8 +53,6 @@ header {
   margin: 0;
   height: 56px;
   padding: 0 16px 0 24px;
-  background-color: #35495E;
-  color: #ffffff;
 }
 
 header span {
